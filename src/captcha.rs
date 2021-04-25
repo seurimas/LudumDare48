847b06129a221363a3682f3713ca24b6e072ce45
basicdays <info@basicdays.com>
2021-04-25 15:33:58 -0500

    texture stuff

--- src/captcha.rs
+++ src/captcha.rs
@@ -10,8 +10,8 @@ use amethyst::renderer::{
 };
 use captcha::{filters::Noise, Captcha};
 
-const WIDTH: u32 = 220;
-const HEIGHT: u32 = 120;
+pub const CAPTCHA_WIDTH: u32 = 220;
+pub const CAPTCHA_HEIGHT: u32 = 120;
 
 pub struct CaptchaData {
     answer: String,
@@ -30,34 +30,23 @@ fn gen_captcha() -> CaptchaData {
     let (answer, image_buffer) = Captcha::new()
         .add_chars(5)
         .apply_filter(Noise::new(0.1))
-        .view(WIDTH, HEIGHT)
+        .view(CAPTCHA_WIDTH, CAPTCHA_HEIGHT)
         .as_tuple()
         .expect("failed to gen captcha");
-    let texture = load_texture(image_buffer);
+    let texture = load_2d_texture(image_buffer, CAPTCHA_WIDTH, CAPTCHA_HEIGHT);
     CaptchaData { answer, texture }
 }
 
-fn load_texture<'a>(image_buffer: Vec<u8>) -> TextureData {
+// yanked from file:///home/basicdays/wip/games/LudumDare48/target/doc/amethyst_rendy/struct.ImageFormat.html
+
+fn load_2d_texture<'a>(image_buffer: Vec<u8>, width: u32, height: u32) -> TextureData {
     let builder = TextureBuilder::new()
-        .with_data_width(WIDTH)
-        .with_data_height(HEIGHT)
-        .with_kind(Kind::D2(WIDTH, HEIGHT, 1, 1))
+        .with_data_width(width)
+        .with_data_height(height)
+        .with_kind(Kind::D2(width, height, 1, 1))
         .with_view_kind(ViewKind::D2)
-        .with_sampler_info(SamplerInfo {
-            min_filter: Filter::Linear,
-            mag_filter: Filter::Linear,
-            mip_filter: Filter::Linear,
-            wrap_mode: (WrapMode::Clamp, WrapMode::Clamp, WrapMode::Clamp),
-            lod_bias: 0.0.into(),
-            lod_range: std::ops::Range {
-                start: 0.0.into(),
-                end: 1000.0.into(),
-            },
-            comparison: None,
-            border: PackedColor(0),
-            anisotropic: Anisotropic::Off,
-            normalized: true,
-        })
+        // TextureBuilder::new already has this...
+        //.with_sampler_info(SamplerInfo::new(Filter::Linear, WrapMode::Clamp))
         .with_raw_data(image_buffer, Format::Rgba8Uint);
     TextureData(builder)
 }
