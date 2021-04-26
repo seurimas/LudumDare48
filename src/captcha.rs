c5828dbf0979ccda548346c74c38cd73b05988b4
basicdays <info@basicdays.com>
2021-04-25 19:54:28 -0500

    spicy captcha

--- src/captcha.rs
+++ src/captcha.rs
@@ -16,7 +16,7 @@ use amethyst::{
         // ImageFormat,
     },
 };
-use captcha::{filters::Noise, Captcha};
+use captcha::{filters, Captcha};
 use image::{self, ImageFormat};
 
 pub const CAPTCHA_COUNT: u32 = 5;
@@ -40,7 +40,11 @@ pub fn get_captchas<'a>(world: &mut World, progress: &'a mut ProgressCounter) ->
 fn gen_captcha<'a>(world: &mut World, progress: &'a mut ProgressCounter) -> CaptchaData {
     let (answer, image_png_buffer) = Captcha::new()
         .add_chars(5)
-        .apply_filter(Noise::new(0.1))
+        .apply_filter(filters::Noise::new(0.6))
+        .apply_filter(filters::Dots::new(10).min_radius(10).max_radius(20))
+        .apply_filter(filters::Cow::new().min_radius(20).max_radius(40))
+        .apply_filter(filters::Wave::new(0.3, 0.6))
+        .set_color([22, 50, 166])
         .view(CAPTCHA_WIDTH, CAPTCHA_HEIGHT)
         .as_tuple()
         .expect("failed to gen captcha");
